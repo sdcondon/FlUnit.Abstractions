d9d38909dfe5fb88dc7ed8be7ab994197723abfb
Simon Condon <simoncondon@hotmail.com>
2024-04-21 11:19:22 +0100

    Drop ITrait and ITraitProvider interfaces in favour of having a concrete attribute type in here. Yes, its an abstractions package, but arguably was overkill to have an abstraction for this - and we will need a common concrete type in order to be able to fix the VSTest adapter so that it loads test projects in a reflection-only context.

---
+++ src/FlUnit.Abstractions/TraitAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace FlUnit
+{
+    /// <summary>
+    /// Attribute used to decorate a test, test class, or assembly with arbitrary name/value pairs ("traits").
+    /// </summary>
+    [AttributeUsage(AttributeTargets.Property | AttributeTargets.Class | AttributeTargets.Assembly, AllowMultiple = true)]
+	public sealed class TraitAttribute : Attribute
+	{
+        /// <summary>
+        /// Creates a new instance of the <see cref="TraitAttribute"/> class.
+        /// </summary>
+        /// <param name="name">The name of the trait.</param>
+        /// <param name="value">The value of the trait.</param>
+        public TraitAttribute(string name, string value) => (Name, Value) = (name, value);
+
+        /// <summary>
+        /// Gets the name of the trait.
+        /// </summary>
+		public string Name { get; }
+
+        /// <summary>
+        /// Gets the value of the trait.
+        /// </summary>
+        public string Value { get; }
+	}
+}
